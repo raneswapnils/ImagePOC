c6f936a8599c216491c9bc0fc36c85f85a95110f
Sameer Rane <Swapnil@time.asia.apple.com>
2016-07-18 01:43:22 +0530

    ImagePOC project added

---
+++ ImagePOC/ImagePOC/DetailViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePOC
+//
+//  Created by Swapnil Rane on 18/07/16.
+//  Copyright © 2016 swapnil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
